ff6421450853490fae3af6f42e4b700aed03c317
altpfwlzh <altpfwlzh@gmail.com>
2022-07-14 22:33:08 +0900

    /register_email_prompt, findpass_email_prompt

---
+++ app/src/main/java/org/appcenter/inudorm/FindPass.kt
@@ -0,0 +1,11 @@
+package org.appcenter.inudorm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FindPass : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_pass)
+    }
+}
--- app/src/main/java/org/appcenter/inudorm/presentation/register/EmailPromptFragment.kt
+++ app/src/main/java/org/appcenter/inudorm/presentation/register/EmailPromptFragment.kt
@@ -1,23 +1,17 @@
 package org.appcenter.inudorm.presentation.register
 
-import android.app.AlertDialog
-import android.content.DialogInterface
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import org.appcenter.inudorm.OnPromptDoneListener
 import org.appcenter.inudorm.R
 import org.appcenter.inudorm.databinding.FragmentEmailPromptBinding
-import org.appcenter.inudorm.util.Event
 import org.appcenter.inudorm.util.eventHandler
 
 class EmailPromptFragment : Fragment() {
@@ -34,7 +28,7 @@ class EmailPromptFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding =
-            DataBindingUtil.inflate(inflater, R.layout.fragment_email_prompt, container, false)
+            DataBindingUtil.inflate(inflater, R.layout.fragment_register_email_prompt, container, false)
         binding.emailViewModel = viewModel
         return binding.root
     }
