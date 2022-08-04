fb57b8a27accef4928c4a2e3e161ae6f35115526
KimWash <ckm0728wash@gmail.com>
2022-08-05 01:32:15 +0900

    FEAT: Add required activity/fragments

---
+++ app/src/main/java/org/appcenter/inudorm/presentation/MateListActivity.kt
@@ -0,0 +1,12 @@
+package org.appcenter.inudorm.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.appcenter.inudorm.R
+
+class MateListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mate_list_activity)
+    }
+}
---
+++ app/src/main/java/org/appcenter/inudorm/presentation/PostDetailActivity.kt
@@ -0,0 +1,15 @@
+package org.appcenter.inudorm.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.appcenter.inudorm.R
+
+/**
+ * 커뮤니티 글 보기 페이지에서 눌러서 들어가는 페이지. PostList
+ */
+class PostDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_detail)
+    }
+}
