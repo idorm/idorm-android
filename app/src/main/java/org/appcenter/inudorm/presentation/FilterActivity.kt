cadb38095dec141fcbdcdeb3624084db2e19082c
altpfwlzh <altpfwlzh@gmail.com>
2022-11-02 20:50:30 +0900

    FEAT : FilterActivity.kt 나이 필터 완료(인디케이터가 따라가기 제외)

--- app/src/main/java/org/appcenter/inudorm/presentation/FilterActivity.kt
+++ app/src/main/java/org/appcenter/inudorm/presentation/FilterActivity.kt
@@ -24,22 +24,40 @@ class FilterActivity : AppCompatActivity() {
         binding.filterViewModel = filterViewModel
 
         val ageDualSeekbar = binding.ageDualSeekbar
-        val ageValLeft = binding.ageValLeft
+        var ageValLeft = binding.ageValLeft
         val ageValRight = binding.ageValRight
 
-
         ageDualSeekbar.setProgress(20f, 40f)
-        ageValLeft.text = ageDualSeekbar.leftSeekBar.toString()
-        ageValRight.text= ageDualSeekbar.progressRight.toString()
+
+        var nowLeftval = ageDualSeekbar.leftSeekBar.progress
+        var nowRigthtval = ageDualSeekbar.rightSeekBar.progress
+
+        ageValLeft.text = checkSeekVal(nowLeftval)
+        ageValRight.text = checkSeekVal(nowRigthtval)
+
+
+
 
         ageDualSeekbar.setOnRangeChangedListener(object: OnRangeChangedListener{
             override fun onRangeChanged(view: RangeSeekBar?, leftValue: Float, rightValue: Float, isFromUser: Boolean) {
-                val padding = ageDualSeekbar.leftSeekBar.indicatorPaddingLeft
-                Log.d(TAG, "onRangeChanged: $padding")
 
-                ageValLeft.text= leftValue.toInt().toString()
-                ageValRight.text= rightValue.toInt().toString()
+                ageValLeft.text= checkSeekVal(leftValue)
+                ageValRight.text = checkSeekVal(rightValue)
 
+                //현재 seekBar 퍼센트 계산
+                var leftPercent = (leftValue.toInt() - 20) * 5 * 0.01
+                var rightPercent = (rightValue.toInt() - 20) * 5 * 0.01
+
+                /*
+                var leftPos = ageDualSeekbar.width * leftPercent
+                var rigthPos = ageDualSeekbar.width * rightPercent
+                
+                ageValLeft?.x = leftPos.toInt().toFloat()
+                ageValRight?.x = rigthPos.toInt().toFloat()
+
+                var x =ageDualSeekbar.x
+                Log.d(TAG, "onRangeChanged: $rigthPos, $rightPercent")
+                */
 
             }
 
@@ -54,4 +72,13 @@ class FilterActivity : AppCompatActivity() {
 
 
     }
+
+    //seekBar의 val값이 40인지 확인하는 함수
+    private fun checkSeekVal(v : Float) : String {
+        var seekbarVal = ""
+        if(v >=40) seekbarVal = "40+"
+        else seekbarVal = v.toInt().toString()
+        return seekbarVal
+    }
+
 }
