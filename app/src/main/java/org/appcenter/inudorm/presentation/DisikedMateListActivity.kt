68186f9e0625fa8e9c6cc52502c7db593d8648e7
altpfwlzh <altpfwlzh@gmail.com>
2022-11-14 20:10:47 +0900

    FEAT : activity_my_information_setting.xml 추가

--- app/src/main/java/org/appcenter/inudorm/presentation/DisikedMateListActivity.kt
+++ app/src/main/java/org/appcenter/inudorm/presentation/DisikedMateListActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.appcenter.inudorm.R
 
-class MateListActivity : AppCompatActivity() {
+class DisikedMateListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_liked_mate_list_activity)
