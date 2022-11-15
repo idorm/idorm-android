a18f93dd14da2aef4ab7255754f48c6390c347f0
altpfwlzh <altpfwlzh@gmail.com>
2022-11-15 16:37:32 +0900

    FEAT : NotificationSettingActivity 추가

---
+++ app/src/main/java/org/appcenter/inudorm/NotificationSettingActivity.kt
@@ -0,0 +1,11 @@
+package org.appcenter.inudorm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NotificationSettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification_setting)
+    }
+}
