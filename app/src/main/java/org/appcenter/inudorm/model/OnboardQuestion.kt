d7e11eaf004b5e1f8285e513a71d63eb8cac12a5
altpfwlzh <altpfwlzh@gmail.com>
2023-03-26 16:32:45 +0900

    FEAT : [Onboard] 글자 수 세기 ui 추가

--- app/src/main/java/org/appcenter/inudorm/model/OnboardQuestion.kt
+++ app/src/main/java/org/appcenter/inudorm/model/OnboardQuestion.kt
@@ -1,10 +1,18 @@
 package org.appcenter.inudorm.model
 
+import android.view.View
+
 
 data class OnboardQuestion(
     var questionText: String,
     var isRequired: String?,
-    var maxLen : Int,
+    var visibility : Int? = View.GONE,
+    var maxLen : String? = "100",
+    var currentLen : String? = "0",
+
+
+
+
     //var validator: (String) -> Boolean
 
 )
--- app/src/main/java/org/appcenter/inudorm/presentation/onboard/BaseInformationFragment.kt
+++ app/src/main/java/org/appcenter/inudorm/presentation/onboard/BaseInformationFragment.kt
@@ -38,12 +38,12 @@ import android.text.Editable
     }
 
     private fun initBind(){
-        val question1 = OnboardQuestion("기상시간을 알려주세요.","(필수)",1)
-        val question2 = OnboardQuestion("정리정돈은 얼마나 하시나요?","(필수)",1)
-        val question3 = OnboardQuestion("샤워는 주로 언제/몇 분 동안 하시나요?","(필수)",1)
-        val question4 = OnboardQuestion("룸메와 연락할 개인 오픈채팅 링크를 알려주세요.","(필수)",1)
-        val question5 = OnboardQuestion("MBTI를 알려주세요.",null,1)
-        val question6 = OnboardQuestion("미래의 룸메에게 하고 싶은 말은?",null,1)
+        val question1 = OnboardQuestion("기상시간을 알려주세요.","(필수)",View.GONE, )
+        val question2 = OnboardQuestion("정리정돈은 얼마나 하시나요?","(필수)",View.GONE)
+        val question3 = OnboardQuestion("샤워는 주로 언제/몇 분 동안 하시나요?","(필수)",View.GONE)
+        val question4 = OnboardQuestion("룸메와 연락할 개인 오픈채팅 링크를 알려주세요.","(필수)",View.GONE)
+        val question5 = OnboardQuestion("MBTI를 알려주세요.",null,View.GONE)
+        val question6 = OnboardQuestion("미래의 룸메에게 하고 싶은 말은?",null,View.VISIBLE,"100", "0")
 
         val list = arrayListOf<OnboardQuestion>(question1, question2, question3, question4, question5, question6)
 
