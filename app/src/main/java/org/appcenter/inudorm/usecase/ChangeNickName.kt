7c54c0c4c311bf9fd557a7145d4fa951096748eb
KimWash <ckm0728wash@gmail.com>
2023-04-05 22:54:44 +0900

    FIX: 파라미터 수정

--- app/src/main/java/org/appcenter/inudorm/usecase/ChangeNickName.kt
+++ app/src/main/java/org/appcenter/inudorm/usecase/ChangeNickName.kt
@@ -4,10 +4,10 @@ import org.appcenter.inudorm.App.Companion.userRepository
 import org.appcenter.inudorm.model.ChangeNickNameDto
 import org.appcenter.inudorm.model.User
 
-class ChangeNickName : UseCase<ChangeNickNameDto, User>() {
+class ChangeNickName : UseCase<ChangeNickNameDto, Unit>() {
 
     // At the top level of your kotlin file:
-    override suspend fun onExecute(params: ChangeNickNameDto): User {
+    override suspend fun onExecute(params: ChangeNickNameDto) {
         return userRepository.changeNickName(params)
     }
 
