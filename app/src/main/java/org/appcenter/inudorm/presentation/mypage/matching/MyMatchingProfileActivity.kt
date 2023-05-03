801d6504a55e6101d4f1fbd421c6a2f34580c013
altpfwlzh <altpfwlzh@gmail.com>
2023-05-03 23:17:21 +0900

    FEAT : [Onboarding] 온보딩 완료

--- app/src/main/java/org/appcenter/inudorm/presentation/mypage/matching/MyMatchingProfileActivity.kt
+++ app/src/main/java/org/appcenter/inudorm/presentation/mypage/matching/MyMatchingProfileActivity.kt
@@ -9,11 +9,16 @@ import kotlinx.coroutines.launch
 import org.appcenter.inudorm.LoadingActivity
 import org.appcenter.inudorm.R
 import org.appcenter.inudorm.databinding.ActivityMyMatchingProfileBinding
+import org.appcenter.inudorm.model.Dorm
+import org.appcenter.inudorm.model.Gender
+import org.appcenter.inudorm.model.JoinPeriod
+import org.appcenter.inudorm.model.OnboardInfo
 import org.appcenter.inudorm.networking.ErrorCode
 import org.appcenter.inudorm.networking.UIErrorHandler
 import org.appcenter.inudorm.presentation.account.OnboardActivity
 import org.appcenter.inudorm.presentation.onboard.BaseInfoPurpose
 import org.appcenter.inudorm.repository.PrefsRepository
+import org.appcenter.inudorm.util.IDormLogger
 import org.appcenter.inudorm.util.OkDialog
 
 class MyMatchingProfileActivity : LoadingActivity() {
@@ -22,21 +27,24 @@ class MyMatchingProfileActivity : LoadingActivity() {
     }
     val viewModel: MyMatchingProfileViewModel by viewModels()
 
+    private val prefsRepository by lazy {
+        PrefsRepository(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.activity = this
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
         viewModel.getMatchingInfo()
-
         lifecycleScope.launch {
             viewModel.myPageState.collect {
                 setLoadingState(it.loading)
                 if (!it.loading && it.error != null) {
                     UIErrorHandler.handle(
                         this@MyMatchingProfileActivity,
-                        PrefsRepository(this@MyMatchingProfileActivity),
-                        it.error,
+                        prefsRepository,
+                        it.error
                     ) { e ->
                         when (e.error) {
                             ErrorCode.MATCHINGINFO_NOT_FOUND -> {
@@ -51,6 +59,25 @@ class MyMatchingProfileActivity : LoadingActivity() {
                             }
                         }
                     }
+                } else if (!it.loading) {
+                    val onboardInfo = OnboardInfo(
+                        dormCategory = it.data?.dormCategory ?: Dorm.DORM1,
+                        gender = it.data?.gender ?: Gender.MALE,
+                        joinPeriod = it.data?.joinPeriod ?: JoinPeriod.WEEK16,
+                        isSnoring = it.data?.isSnoring ?: false,
+                        isGrinding = it.data?.isGrinding ?: false,
+                        isSmoking = it.data?.isSmoking ?: false,
+                        isAllowedFood = it.data?.isAllowedFood ?: false,
+                        isWearEarphones = it.data?.isWearEarphones ?: false,
+                        age = it.data?.age ?: 20,
+                        wakeupTime = it.data?.wakeUpTime ?: "",
+                        cleanUpStatus = it.data?.cleanUpStatus ?: "",
+                        showerTime = it.data?.showerTime ?: "",
+                        openKakaoLink = it.data?.openKakaoLink ?: "",
+                        mbti = it.data?.mbti ?: "",
+                        wishText = it.data?.wishText ?: "",
+                    )
+                    prefsRepository.setMatchingInfo(onboardInfo)
                 }
             }
         }
