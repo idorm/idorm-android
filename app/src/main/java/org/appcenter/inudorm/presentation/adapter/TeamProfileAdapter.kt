4df9add35b23874fec673fd65875d7df14948d38
altpfwlzh <altpfwlzh@gmail.com>
2023-07-14 20:45:12 +0900

    FEAT : [teamCalendar] TeamProfileAdapter

---
+++ app/src/main/java/org/appcenter/inudorm/presentation/adapter/TeamProfileAdapter.kt
@@ -0,0 +1,49 @@
+package org.appcenter.inudorm.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import org.appcenter.inudorm.R
+import org.appcenter.inudorm.databinding.ItemTeamProfileBinding
+import org.appcenter.inudorm.model.TeamProfile
+
+
+class TeamProfileAdapter(
+    private var _dataSet: ArrayList<TeamProfile>
+) :
+    RecyclerView.Adapter<TeamProfileAdapter.TeamProfileViewHolder>() {
+
+
+    var dataSet: ArrayList<TeamProfile>
+        get() = _dataSet
+        set(value) {
+            _dataSet = value
+        }
+
+    inner class TeamProfileViewHolder(
+        var viewBinding: ItemTeamProfileBinding
+
+    ) : RecyclerView.ViewHolder(viewBinding.root) { }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): TeamProfileViewHolder {
+
+        val binding = DataBindingUtil.inflate<ItemTeamProfileBinding>(
+            LayoutInflater.from(viewGroup.context),
+            R.layout.item_team_profile,
+            viewGroup,
+            false
+        )
+
+        return TeamProfileViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(viewHolder: TeamProfileViewHolder, position: Int) {
+
+        viewHolder.viewBinding.teamProfile = _dataSet[position]
+
+    }
+
+    override fun getItemCount() = dataSet.size
+
+}
