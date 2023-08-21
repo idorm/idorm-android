ac8cadcc36074544a403ecfb11adae4f1d0956c4
KimWash <ckm0728wash@gmail.com>
2023-08-21 17:45:34 +0900

    FIX: 로딩 프래그먼트 모든 로딩 액티비티에 대응

--- app/src/main/java/org/appcenter/inudorm/presentation/LoadingFragment.kt
+++ app/src/main/java/org/appcenter/inudorm/presentation/LoadingFragment.kt
@@ -5,16 +5,17 @@ import android.os.Bundle
 import android.widget.ProgressBar
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import org.appcenter.inudorm.LoadingActivity
 import org.appcenter.inudorm.R
 
 open class LoadingFragment : Fragment() {
 
     fun setLoadingState(loading: Boolean) {
-        (this@LoadingFragment.requireActivity() as MainActivity).setLoadingState(loading)
+        (requireActivity() as LoadingActivity).setLoadingState(loading)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        
+
     }
 }
