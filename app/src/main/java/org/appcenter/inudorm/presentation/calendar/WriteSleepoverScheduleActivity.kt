934cc64ec201fc951b701a52fb150c657a935e6b
altpfwlzh <altpfwlzh@gmail.com>
2023-08-22 00:26:15 +0900

    FEAT : 외박 일정 페이지 글자까지만 생성

---
+++ app/src/main/java/org/appcenter/inudorm/presentation/calendar/WriteSleepoverScheduleActivity.kt
@@ -0,0 +1,12 @@
+package org.appcenter.inudorm.presentation.calendar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.appcenter.inudorm.R
+
+class WriteSleepoverScheduleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_write_sleepover_schedule)
+    }
+}
